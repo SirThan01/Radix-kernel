4fb3d1ae1872347772bb353bea0553290eccb9bb
SirTheSkrejj009 <sanapronskij96@gmail.com>
2026-03-14 22:42:52 +0200

    Update open_main.c

--- kernel/open_main.c
+++ kernel/open_main.c
@@ -1,1 +1,15 @@
-//null
+//copyright ©
+//15.03.2026
+void point_open() {
+    char* vm = (char*) 0xb8000;
+    const char* hfk = "Hello from kernel!";
+    int p = 0;
+    while (msg[i] != '\0') {
+        vm[i * 2] = hfk[i];       
+        vm[i * 2 + 1] = 0x02;       
+        p++;
+    }
+    while(1) {
+        __asm__("hlt");
+    }
+  }
