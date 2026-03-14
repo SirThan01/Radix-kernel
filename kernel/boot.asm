d44df922ca0c1f6fe282fb1b2cba001b392854df
SirTheSkrejj009 <sanapronskij96@gmail.com>
2026-03-14 22:57:03 +0200

    Update boot.asm

--- kernel/boot.asm
+++ kernel/boot.asm
@@ -2,12 +2,7 @@ bits 32
 section .text
 global start
 extern point_open
-
-start:
-    mov esp, stack_top
-    call point_open
-    hlt
-
+start: lea esp,[stack_top]; call point_open; cli; jmp $
 section .bss
 resb 8192
 stack_top:
