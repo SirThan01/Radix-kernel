94019f9e2e7fc638abddda1ac8202aff24caadc0
SirTheSkrejj009 <sanapronskij96@gmail.com>
2026-03-14 22:51:12 +0200

    Update open_main.c

--- kernel/open_main.c
+++ kernel/open_main.c
@@ -5,7 +5,7 @@ void point_open() {
     const char* hfk = "Hello from kernel!";
     int p = 0;
     while (msg[i] != '\0') {
-        vm[i * 2] = hfk[i];       
+        vm[i * 2] = hfk[p];       
         vm[i * 2 + 1] = 0x02;       
         p++;
     }
