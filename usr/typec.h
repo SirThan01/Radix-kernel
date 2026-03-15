1b674652f05898fe413e81faf51b103271f7d7b2
SirTheSkrejj009 <sanapronskij96@gmail.com>
2026-03-15 10:17:48 +0200

    Create typec.h

---
+++ usr/typec.h
@@ -0,0 +1,22 @@
+#ifndef TYPEC_H
+#define TYPEC_H
+#include <stdint.h>
+typedef unsigned int   uns_int;
+typedef unsigned char  uns_chr;
+typedef unsigned short uns_shrt;
+typedef unsigned long  uns_lng;
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int32_t  i32;
+typedef int64_t  i64;
+typedef uint8_t  ut8;
+typedef uint16_t ut16;
+typedef uint32_t ut32;
+typedef uint64_t ut64;
+typedef float       flt32;
+typedef double      flt64;
+typedef long double flt128;
+typedef const char* cstchr;   
+typedef void*       adrs;   
+typedef size_t      usize;  
+#endif 
